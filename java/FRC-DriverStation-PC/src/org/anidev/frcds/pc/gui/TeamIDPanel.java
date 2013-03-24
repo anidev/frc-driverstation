ba628c988bdd565e955d4c1eb058638a4037a470
anidev <anidev.aelico@gmail.com>
2013-03-24 16:07:06 -0400

    Added panel for changing team id

---
+++ java/FRC-DriverStation-PC/src/org/anidev/frcds/pc/gui/TeamIDPanel.java
@@ -0,0 +1,69 @@
+package org.anidev.frcds.pc.gui;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import java.awt.Component;
+import javax.swing.JFormattedTextField;
+import javax.swing.text.DefaultFormatter;
+import javax.swing.text.DefaultFormatterFactory;
+import javax.swing.text.NumberFormatter;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.text.NumberFormat;
+
+public class TeamIDPanel extends JPanel {
+	/**
+	 * Create the panel.
+	 */
+	public TeamIDPanel() {
+		setSize(new Dimension(170,40));
+		GridBagLayout gridBagLayout=new GridBagLayout();
+		gridBagLayout.columnWidths=new int[] {0,0};
+		gridBagLayout.rowHeights=new int[] {0};
+		gridBagLayout.columnWeights=new double[] {0.0,0.0};
+		gridBagLayout.rowWeights=new double[] {0.0};
+		setLayout(gridBagLayout);
+
+		JLabel teamIDLabel=new JLabel("Team ID");
+		teamIDLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		GridBagConstraints gbc_teamIDLabel=new GridBagConstraints();
+		gbc_teamIDLabel.gridx=0;
+		gbc_teamIDLabel.insets=new Insets(0,0,0,5);
+		gbc_teamIDLabel.anchor=GridBagConstraints.WEST;
+		gbc_teamIDLabel.gridy=0;
+		add(teamIDLabel,gbc_teamIDLabel);
+
+		NumberFormat format=NumberFormat.getIntegerInstance();
+		format.setMinimumIntegerDigits(1);
+		format.setMaximumIntegerDigits(4);
+		format.setGroupingUsed(false);
+
+		JFormattedTextField teamIDField=new JFormattedTextField(format);
+		teamIDField.setColumns(4);
+		GridBagConstraints gbc_teamIDField=new GridBagConstraints();
+		gbc_teamIDField.weightx=1.0;
+		gbc_teamIDField.fill=GridBagConstraints.HORIZONTAL;
+		gbc_teamIDField.gridx=1;
+		gbc_teamIDField.gridy=0;
+
+		teamIDField.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				KeyboardFocusManager.getCurrentKeyboardFocusManager()
+						.clearGlobalFocusOwner();
+			}
+		});
+
+		add(teamIDField,gbc_teamIDField);
+	}
+}
