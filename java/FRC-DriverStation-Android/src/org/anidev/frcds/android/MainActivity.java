588d105e14c4db5d3fd2df5dbe265cb72f5908f8
anidev <anidev.aelico@gmail.com>
2013-09-05 15:04:04 -0400

    Minor misc changes to Android app

--- java/FRC-DriverStation-Android/src/org/anidev/frcds/android/MainActivity.java
+++ java/FRC-DriverStation-Android/src/org/anidev/frcds/android/MainActivity.java
@@ -1,21 +1,24 @@
 package org.anidev.frcds.android;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	protected void onCreate(Bundle savedState) {
+		super.onCreate(savedState);
+		ActionBar actionBar=getSupportActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.setDisplayOptions(0);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main,menu);
+		getSupportMenuInflater().inflate(R.menu.activity_main,menu);
 		return true;
 	}
 
---
+++ java/FRC-DriverStation-Android/src/org/anidev/frcds/android/OperationFragment.java
@@ -0,0 +1,20 @@
+package org.anidev.frcds.android;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+public class OperationFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedState) {
+		super.onCreate(savedState);
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedState) {
+//		return inflater.inflate(R.layout.,container,false);
+		return null;
+	}
+}
