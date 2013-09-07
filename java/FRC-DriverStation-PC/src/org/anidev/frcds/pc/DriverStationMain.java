400da39e8f5652e3b5717eff49bb879801316eeb
Anirudh B <anidev.aelico@gmail.com>
2013-09-07 12:54:24 -0400

    PC DS Main does not need loop hertz constant

--- java/FRC-DriverStation-PC/src/org/anidev/frcds/pc/DriverStationMain.java
+++ java/FRC-DriverStation-PC/src/org/anidev/frcds/pc/DriverStationMain.java
@@ -9,7 +9,6 @@ import org.anidev.frcds.pc.battery.win.WindowsBatteryProvider;
 import org.anidev.frcds.pc.gui.DriverStationFrame;
 
 public class DriverStationMain {
-	public static final double LOOP_HERTZ=50.0;
 	private static DriverStationFrame dsFrame;
 	private static DriverStation ds;
 
