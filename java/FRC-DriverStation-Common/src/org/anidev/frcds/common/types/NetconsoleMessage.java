a1c236c779f8099043e0145926e3af393ee201b2
anidev <anidev.aelico@gmail.com>
2013-11-07 18:47:53 -0500

    Created NetconsoleMessage class

---
+++ java/FRC-DriverStation-Common/src/org/anidev/frcds/common/types/NetconsoleMessage.java
@@ -0,0 +1,37 @@
+package org.anidev.frcds.common.types;
+
+import java.util.Calendar;
+
+public class NetconsoleMessage {
+	private final Type type;
+	private final String message;
+	private final Calendar date;
+
+	public NetconsoleMessage(Type type,String message) {
+		this.type=type;
+		this.message=message;
+		this.date=Calendar.getInstance();
+		date.setTimeInMillis(System.currentTimeMillis());
+	}
+	public NetconsoleMessage(Type type,String message,Calendar date) {
+		this.type=type;
+		this.message=message;
+		this.date=date;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public enum Type {
+		TODS,TOROBOT;
+	}
+}
