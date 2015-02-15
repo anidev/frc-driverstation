fa9207f1052ec3bd58ea98c0eac1c036d2220f37
Anirudh Bagde <anidev.aelico@gmail.com>
2015-02-15 18:32:56 -0500

    Documentation for FRCCommunicationListener Getting this commit out of the way before the major protocol overhaul

--- java/FRCDS-Java-Comm/src/main/java/org/anidev/frcds/proto/FRCCommunicationListener.java
+++ java/FRCDS-Java-Comm/src/main/java/org/anidev/frcds/proto/FRCCommunicationListener.java
@@ -1,5 +1,22 @@
 package org.anidev.frcds.proto;
 
+import org.anidev.frcds.proto.tods.FRCRobotControl;
+import org.anidev.frcds.proto.torobot.FRCCommonControl;
+
+/**
+ * Listener interface for receiving data processed by the FRCCommunication
+ * class. It can be used for receiving data from both the robot and the driver
+ * station.
+ * 
+ * @author Anirudh Bagde
+ */
 public interface FRCCommunicationListener {
+	/**
+	 * Called when data is received and deserialized.
+	 * 
+	 * @param data
+	 *            The parsed data object. This will probably be either
+	 *            {@link FRCCommonControl} or {@link FRCRobotControl}.
+	 */
 	public void receivedData(CommData data);
 }
